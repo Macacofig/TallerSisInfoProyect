c9f7eccf09cf3f6afeb9fcc97465dabac22553e8
Nataly <nramirezmachicado@gmail.com>
2026-09-05 20:53:19 -0400

    UH-0: Implementar layout

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ApiService } from './services/api';
+import { Sidebar } from '../layout/sidebar/sidebar';
+
+import { Navbar } from '../layout/navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Sidebar, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
